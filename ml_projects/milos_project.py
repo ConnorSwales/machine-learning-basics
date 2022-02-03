f40708fb9451b6ea60fe815be7920008d9ccebbb
MilosIndjin98 <67111896+MilosIndjin98@users.noreply.github.com>
2022-02-03 13:00:42 +0000

    custom modules
    fixed modules folder

--- ml_projects/milos_project.py
+++ ml_projects/milos_project.py
@@ -1,1 +1,18 @@
 # project: use big data ?? to find penis enlargening chemicals
+
+# import numpy as np
+# import scipy as sp
+import sys
+sys.path.insert(1, 'modules')
+
+import pandas as pd
+import sklearn as sk
+
+import os.path as osp
+
+import basic_functions as funcs
+
+star_data_fp = osp.join("resources", "datasets", "star_classification.csv")
+star_data_raw = pd.read_csv(star_data_fp)
+
+funcs.print_info(star_data_raw)
