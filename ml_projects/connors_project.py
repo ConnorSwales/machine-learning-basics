83a03521df18d95d98ab6eff55ea45acf7a02d0e
ConnorSwales <connor.swales97@gmail.com>
2022-02-03 13:24:57 +0000

    Update connors_project.py

--- ml_projects/connors_project.py
+++ ml_projects/connors_project.py
@@ -1,14 +1,19 @@
-# import numpy as np
-# import scipy as sp
-import sys
-sys.path.insert(1, 'modules')
-
+# Main external library imports for maths
+import numpy as np
+import scipy as sp
 import pandas as pd
-# import sklearn as sk
+import sklearn as sk
 
+# Auxillary library imports for ease of use
 import os.path as osp
 
+# Link modules folder for functions
+import sys
+sys.path.append('modules')
+
+# Import functions from modules folder
 import basic_functions as funcs
+
 
 star_data_fp = osp.join("resources", "datasets", "star_classification.csv")
 star_data_raw = pd.read_csv(star_data_fp)
